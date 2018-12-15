0021e340f542095cb10a7697af8c842ea86861f6
Acaua Sperl de Faria <acaua.faria@gmail.com>
2018-12-15 18:40:10 -0200

    Fetch and parse address data

--- routes/gasStations.js
+++ routes/gasStations.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 
 const router = express.Router();
 
@@ -10,7 +11,39 @@ router.get("/", (req, res) => {
       error_message: "Invalid request. Missing the 'latlng' parameter."
     });
 
-  return res.json({ latlng });
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&sensor=false&key=AIzaSyA6TfU84r6wT2gu1NYAOCN7JkO342K21So`
+    )
+    .then(function(response) {
+      const result = response.data.results[0];
+
+      const address = result.address_components.reduce((address, component) => {
+        if (component.types.includes("administrative_area_level_1")) {
+          return { ...address, estado: component.short_name };
+        } else if (component.types.includes("administrative_area_level_2")) {
+          return { ...address, cidade: component.long_name };
+        } else if (component.types.includes("sublocality_level_1")) {
+          return { ...address, bairro: component.long_name };
+        } else if (component.types.includes("route")) {
+          return { ...address, logradouro: component.long_name };
+        } else if (component.types.includes("street_number")) {
+          return { ...address, numero: component.short_name };
+        } else if (component.types.includes("postal_code")) {
+          return { ...address, cep: component.short_name };
+        }
+
+        return address;
+      }, {});
+
+      const data = {
+        lat: result.geometry.location.lat,
+        lng: result.geometry.location.lng,
+        endereco: address
+      };
+
+      return res.json(data);
+    });
 });
 
 module.exports = router;
