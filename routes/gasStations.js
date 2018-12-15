fdd9045e56dd96962532e9e985adbf9e999941e1
Acaua Sperl de Faria <acaua.faria@gmail.com>
2018-12-15 17:39:33 -0200

    Parsing latlng

--- routes/gasStations.js
+++ routes/gasStations.js
@@ -3,7 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.json({ hello: "world" });
+  const { latlng } = req.query;
+
+  if (!latlng)
+    return res.status(400).json({
+      error_message: "Invalid request. Missing the 'latlng' parameter."
+    });
+
+  return res.json({ latlng });
 });
 
 module.exports = router;
